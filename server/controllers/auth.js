007a1d011b9f2ccae0ed4e28a5ffa3f3f3539d33
Pulipati Keerthi <pkeerthi1929@gmail.com>
2023-01-19 22:06:52 +0530

    first commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,8 +21,8 @@ export const signup = async (req,res) => {
             email,
             password: hashedPassword,
         });
-           console.log(newUser)
-        const token = jwt.sign({ email: newUser.email, id: newUser._id },process.env.JWT_SECRET, { expiresIn: '1h' });
+         
+        const token = jwt.sign({ email: newUser.email, id: newUser._id },process.env.JWT_SECERET, { expiresIn: '1h' });
 
         res.status(200).json({ result: newUser, token })
     }
@@ -44,7 +44,7 @@ export const login = async (req,res) => {
         if(!isPasswordCrt){
             return res.status(400).json({message : "Invalid credentials"})
         }
-        const token = jwt.sign({ email : existinguser.email, id:existinguser._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
+        const token = jwt.sign({ email : existinguser.email, id:existinguser._id}, process.env.JWT_SECERET, { expiresIn: '1h'});
         res.status(200).json({ result: existinguser, token })
     }catch (error) {
         res.status(500).json("Something went wrong...")
